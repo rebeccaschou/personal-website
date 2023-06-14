5fdc8a324f314ae9625692e8da6ae85b7763a31c
rebeccaschou <rstchou@gmail.com>
2023-06-14 11:34:08 -0400

    Redesign about details page layout

--- src/components/About/AboutDetails/AboutDetails.jsx
+++ src/components/About/AboutDetails/AboutDetails.jsx
@@ -4,49 +4,42 @@ import styles from "./AboutDetails.module.scss";
 export default function AboutDetails() {
   return (
     <div className={styles[`about-details`]} id="about-details">
-      <div className={styles.card}>
-        <h1>About Me</h1>
-        <div className={styles[`top-details`]}>
-          <div className={styles.left}>
-            <img
-              src="assets/profile-photo-details.png"
-              alt="profile"
-              className={styles[`about-details-image`]}
-            ></img>
-          </div>
-          <div className={styles.summary}>
-            <h2 className={styles.header}>FULL NAME</h2>
-            <p className={styles.subtext}>Rebecca Chou</p>
-            <h2 className={styles.header}>EMAIL</h2>
-            <p className={styles.subtext}>rebecca_chou@brown.edu</p>
-            <h2 className={styles.header}>LINKEDIN</h2>
-            <a
-              href="https://www.linkedin.com/in/rebeccaschou/"
-              className={styles.subtext}
-            >
-              https://www.linkedin.com/in/rebeccaschou/
-            </a>
-            <h2 className={styles.header}>GITHUB</h2>
-            <a
-              href="https://github.com/rebeccaschou"
-              className={styles.subtext}
-            >
-              https://github.com/rebeccaschou
-            </a>
-          </div>
-        </div>
+      <div className={styles.left}>
+        <h1 classNames={styles.greeting}>Hi, I'm Rebecca!</h1>
         <p className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          I'm a sophomore studying computer science on a software track at Brown
+          University. I'm interested in the intersection of social justice and
+          technology. Outside of class, I'm co-mentorship chair for Brown's
+          Women in Computer Science (WiCS) organization and a teaching assistant
+          for the Data Structures and Algorithms class (CSCI0200). <br></br>
+          <br></br> In my free time, I love to crochet, care for my houseplants,
+          and compete on the Brown Taekwondo team. I'm currently learning how to
+          make amigurumi birds! Please feel free to reach out to me at
+          rebecca_chou@brown.edu if you’d like to chat!
         </p>
+        <div className={styles.summary}>
+          <h2 className={styles.header}>LINKEDIN</h2>
+          <a
+            href="https://www.linkedin.com/in/rebeccaschou/"
+            className={styles.subtext}
+          >
+            linkedin.com/in/rebeccaschou
+          </a>
+          <h2 className={styles.header}>GITHUB</h2>
+          <a href="https://github.com/rebeccaschou" className={styles.subtext}>
+            github.com/rebeccaschou
+          </a>
+        </div>
         <button className={styles[`resume-button`]}>
           <a href="https://github.com/rebeccaschou">Download Resume</a>
         </button>
+      </div>
+      <div className={styles.right}>
+        <img
+          src="assets/profile-photo-details.png"
+          alt="profile"
+          className={styles[`about-details-image`]}
+        ></img>
       </div>
     </div>
   );
