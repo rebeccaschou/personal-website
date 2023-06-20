a46d0345c39697898ae0ff210cb8aa229cad445e
rebeccaschou <rstchou@gmail.com>
2023-06-20 13:52:05 -0400

    Polish projects page mobile compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       timeline: "Jun - Aug 2023 (14 wks)",
       tools: ["React", "Django", "FIGMA"],
       description:
-        "Designing a personal website (that you're looking at right now!) to show personal projects, publications, and for-fun crafts!",
+        "Designed a personal website (that you're looking at right now!) to show personal projects, publications, and for-fun crafts! Indepdendently learned React and related tools, such as ReactRouter.",
       descriptionExtended:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       image: website,
