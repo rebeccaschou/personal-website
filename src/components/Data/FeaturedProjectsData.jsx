ef1bd24130c6e2725a9136f332f61e2688ca1e38
rebeccaschou <rstchou@gmail.com>
2025-06-09 19:23:20 -0400

    Add responsive layout to projects

--- src/components/Data/FeaturedProjectsData.jsx
+++ src/components/Data/FeaturedProjectsData.jsx
@@ -12,11 +12,11 @@ const majorProjects = [
     timeline: "",
     tools: ["XCode", "Swift", "Core Data"],
     description:
-      "Designed and programmed mobile app in Swift to standardize syllabus and track martial arts practices, tournament information, and class attendance.",
-    descriptionExtended: [
-      "Created as a centralized source of information for the World Tang Soo Do Organization, the Tang Soo Do App provides a standardized syllabus, rank requirements, and journal feature for Tang Soo Do students. Each user’s account is personalized, only displaying the syllabus requirements specific to their rank, and logged practice sessions, classes, competitions, and tests appear in their personal martial arts journal with customizable notes.",
-      "Through this project, I learned to incorporate the Core Data framework to allow for data persistence of syllabus information, as well as Apple’s UserDefaults functionality to store individual user information. In addition, I had the opportunity to collaborate with Tang Soo Do studio owners to establish which features and layouts would be most beneficial. Tang Soo!",
-    ],
+      "Built a sandbox social media platform to study the impact of user roles on experiences of social support",
+    // descriptionExtended: [
+    //   "Created as a centralized source of information for the World Tang Soo Do Organization, the Tang Soo Do App provides a standardized syllabus, rank requirements, and journal feature for Tang Soo Do students. Each user’s account is personalized, only displaying the syllabus requirements specific to their rank, and logged practice sessions, classes, competitions, and tests appear in their personal martial arts journal with customizable notes.",
+    //   "Through this project, I learned to incorporate the Core Data framework to allow for data persistence of syllabus information, as well as Apple’s UserDefaults functionality to store individual user information. In addition, I had the opportunity to collaborate with Tang Soo Do studio owners to establish which features and layouts would be most beneficial. Tang Soo!",
+    // ],
     image: chirp,
     alt: "chirp",
   },
@@ -29,8 +29,8 @@ const majorProjects = [
     timeline: "Jun - Aug 2023 (14 wks)",
     tools: ["React", "Django", "FIGMA"],
     description:
-      "Designed a personal website (that you're looking at right now!) to show personal projects, publications, and for-fun crafts! Indepdendently learned React and related tools, such as ReactRouter.",
-    descriptionExtended: ["Extended description coming soon!"],
+      "Designed and built a personal portfolio website (that you're browsing right now!)",
+    // descriptionExtended: ["Extended description coming soon!"],
     image: website,
     alt: "personal website",
   },
@@ -43,11 +43,11 @@ const majorProjects = [
     timeline: "Feb - May 2022 (16 wks)",
     tools: ["XCode", "Swift", "Core Data"],
     description:
-      "Designed and programmed mobile app in Swift to standardize syllabus and track martial arts practices, tournament information, and class attendance.",
-    descriptionExtended: [
-      "Created as a centralized source of information for the World Tang Soo Do Organization, the Tang Soo Do App provides a standardized syllabus, rank requirements, and journal feature for Tang Soo Do students. Each user’s account is personalized, only displaying the syllabus requirements specific to their rank, and logged practice sessions, classes, competitions, and tests appear in their personal martial arts journal with customizable notes.",
-      "Through this project, I learned to incorporate the Core Data framework to allow for data persistence of syllabus information, as well as Apple’s UserDefaults functionality to store individual user information. In addition, I had the opportunity to collaborate with Tang Soo Do studio owners to establish which features and layouts would be most beneficial. Tang Soo!",
-    ],
+      "Designed and built a mobile app to support training of World Tang Soo Do Association practicioners",
+    // descriptionExtended: [
+    //   "Created as a centralized source of information for the World Tang Soo Do Organization, the Tang Soo Do App provides a standardized syllabus, rank requirements, and journal feature for Tang Soo Do students. Each user’s account is personalized, only displaying the syllabus requirements specific to their rank, and logged practice sessions, classes, competitions, and tests appear in their personal martial arts journal with customizable notes.",
+    //   "Through this project, I learned to incorporate the Core Data framework to allow for data persistence of syllabus information, as well as Apple’s UserDefaults functionality to store individual user information. In addition, I had the opportunity to collaborate with Tang Soo Do studio owners to establish which features and layouts would be most beneficial. Tang Soo!",
+    // ],
     image: tangsoodo,
     alt: "martial arts app",
   },
--- src/components/Projects/Project/ProjectText/ProjectText.jsx
+++ src/components/Projects/Project/ProjectText/ProjectText.jsx
@@ -4,8 +4,8 @@ import ProjectButton from "../ProjectButton/ProjectButton";
 export default function ProjectText(props) {
   return (
     <div className={styles.text}>
+      <p className={styles.tags} style={{ textTransform: 'uppercase' }}>{props.tags.join(" | ")}</p>
       <h1 className={styles.header}>{props.title}</h1>
-      <p className={styles.tags}>{props.tags.join(" | ")}</p>
       <p className={styles.description}>{props.description}</p>
       <ProjectButton color={props.color} id={props.id} />
     </div>
