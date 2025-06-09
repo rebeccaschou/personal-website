72ba8f92876820adedea5e337fef14be34b74da4
rebeccaschou <rstchou@gmail.com>
2025-06-09 16:15:45 -0400

    Fix references to global style

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -5,8 +5,15 @@ export default function BottomBar() {
     <div className={styles[`bottom-bar`]}>
       <div className={styles.container}>
         <hr className={styles.divider}></hr>
-        <div>
-          <p>© Rebecca Chou 2025</p>
+        <div className={styles.text}>
+          <div className={styles.copyright}>
+            <p>© Rebecca Chou 2025</p>
+          </div>
+          <div className={styles.contacts}>
+            <a href="https://github.com/rebeccaschou" rel="noreferrer" target="_blank" className={styles.contact}>GitHub</a>
+            <a href="https://www.linkedin.com/in/rebeccaschou/" rel="noreferrer" target="_blank" className={styles.contact}>LinkedIn</a>
+            <a href="https://drive.google.com/file/d/1KIhP6idzTK5od9DOWZOdo4_GsjSvzsRD/view?usp=drive_link" rel="noreferrer" target="_blank" className={styles.contact}>Resume</a>
+          </div>
         </div>
       </div>
     </div>
